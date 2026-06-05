72c5430b030238558b5cf724c23d4c2ffecee47b
Valon Tufa <valontufa@gmail.com>
2026-06-05 20:12:34 +0200

    Remove leftover Google Fonts preconnect hints (fonts are self-hosted)

--- header.php
+++ header.php
@@ -4,9 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Senior Full Stack Developer &amp; Tech Lead with 7+ years of experience in PHP, WordPress, Laravel and WooCommerce. Open to remote roles.">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <?php wp_head(); ?>
+<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
